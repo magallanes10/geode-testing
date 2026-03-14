c389c163f6a341b41c8ad3ed4afdfaa91785c278
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2026-03-14 13:32:48 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,54 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
-
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
-using namespace geode::prelude;
-
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
 #include <Geode/modify/MenuLayer.hpp>
+#include <cocos2d.h>
+#include <network/HttpClient.h>
+#include <json.hpp> // for JSON parsing
+using json = nlohmann::json;
+
+using namespace geode::prelude;
+using namespace cocos2d::network;
+
 class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+        // Create the popup automatically
+        fetchAndShowPopup();
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+        return true;
+    }
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    void fetchAndShowPopup() {
+        auto request = new HttpRequest();
+        request->setUrl("https://pastebin.com/raw/JiRtFNBG");
+        request->setRequestType(HttpRequest::Type::GET);
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        request->setResponseCallback([this](HttpClient* client, HttpResponse* response) {
+            if (!response || !response->isSucceed()) {
+                log::warn("Failed to fetch remote popup message!");
+                return;
+            }
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+            std::vector<char> *buffer = response->getResponseData();
+            std::string data(buffer->begin(), buffer->end());
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+            try {
+                auto jsonData = json::parse(data);
+                std::string message = jsonData["message"];
+                std::string from = jsonData["from"];
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+                // Add RGDPS announcement
+                std::string fullMessage = message + "\n\nFrom: " + from + "\n[RGDPS Announcement]";
+
+                // Show the popup
+                FLAlertLayer::create("Geode", fullMessage.c_str(), "OK")->show();
+
+            } catch (std::exception& e) {
+                log::warn("Failed to parse JSON: {}", e.what());
+            }
+        });
+
+        HttpClient::getInstance()->send(request);
+        request->release();
+    }
 };
