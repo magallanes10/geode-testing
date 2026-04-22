3248f873a93ef13dd0a778d713619153dd54b05a
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2026-04-22 08:14:37 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,52 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
 
+// We use a global variable to track if the announcement has already been shown
+// this session so it doesn't pop up every time you return to the menu.
+bool G_ANNOUNCEMENT_SHOWN = false;
+
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
-        if (!MenuLayer::init())
-            return false;
+        if (!MenuLayer::init()) return false;
 
-        // Find the bottom menu
-        auto menuNode = this->getChildByID("bottom-menu");
-        if (!menuNode)
-            return true;
-
-        // Create button with GD info icon
-        auto myButton = CCMenuItemSpriteExtra::create(
-            CCSprite::createWithSpriteFrameName("GJ_info_001.png"), // info icon
-            this,
-            menu_selector(MyMenuLayer::onMyButton)
-        );
-
-        // Unique ID for layout
-        myButton->setID("my-popup-button"_spr);
-
-        // Add button to bottom menu
-        menuNode->addChild(myButton);
-        menuNode->updateLayout(); // ensures proper placement
+        // Only fetch and show if it hasn't been shown yet this session
+        if (!G_ANNOUNCEMENT_SHOWN) {
+            this->fetchAnnouncement();
+        }
 
         return true;
     }
 
-    // Button callback
-    void onMyButton(CCObject*) {
-        FLAlertLayer::create(
-            "RGDPS Announcement",                   // title
-            "Hello there bruhh!\nThis is your message.", // content
-            "OK"                                     // button text
-        )->show();
+    void fetchAnnouncement() {
+        // Fetching from your Pastebin link
+        web::WebRequest()
+            .get("https://pastebin.com/raw/vRNYgwyL")
+            .listen([this](web::WebResponse* res) {
+                if (res && res->ok()) {
+                    auto data = res->json();
+                    
+                    if (data && data.value().contains("announcement")) {
+                        auto ann = data.value()["announcement"];
+                        
+                        std::string title = ann["title"].as_string();
+                        std::string context = ann["context"].as_string();
+
+                        // Show the popup on the main thread
+                        FLAlertLayer::create(
+                            title.c_str(), 
+                            context.c_str(), 
+                            "OK"
+                        )->show();
+
+                        // Mark as shown so it doesn't spam the user
+                        G_ANNOUNCEMENT_SHOWN = true;
+                    }
+                } else {
+                    log::error("Failed to fetch GDPS announcement");
+                }
+            });
     }
 };
